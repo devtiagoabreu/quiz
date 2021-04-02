c9ba33f6559550b9aed8b8017b472eeafad2a945
Tiago de Abreu <devtiagoabreu@gmail.com>
2021-04-02 07:31:50 -0300

    End Game Page

--- game.js
+++ game.js
@@ -12,28 +12,28 @@ let availableQuestions = []
 
 let questions = [
   {
-    question: 'What is 2 + 2',
-    choice1: '2',
-    choice2: '4',
-    choice3: '21',
-    choice4: '17',
-    answer: 2,
+    question: 'Qual é o melhor amigo do Irmão do Jorel?',
+    choice1: 'Marcinho',
+    choice2: 'Billy Doidão',
+    choice3: 'Pablito',
+    choice4: 'Beto Cachinhos',
+    answer: 1,
   },
   {
-    question: 'What is 2 + 2',
-    choice1: '2',
-    choice2: '4',
-    choice3: '21',
-    choice4: '17',
-    answer: 2,
+    question: 'No epsódio "Caçadores da Figurinha Perdida", qual é o nº da figurinha perdida?',
+    choice1: '1',
+    choice2: '100',
+    choice3: '900',
+    choice4: '9001',
+    answer: 4,
   },
   {
-    question: 'What is 2 + 2',
-    choice1: '2',
-    choice2: '4',
-    choice3: '21',
-    choice4: '17',
-    answer: 2,
+    question: 'Qual o nome da escola onde o Irmão do Jorel estuda?',
+    choice1: 'Elefante de Porcelana',
+    choice2: 'Zazazila',
+    choice3: 'Escolinha da Lola',
+    choice4: 'Pônei Encantado',
+    answer: 4,
   },
   {
     question: 'What is 2 + 2',
@@ -54,7 +54,7 @@ let questions = [
 ]
 
 const SCORE_POINTS = 100
-const MAX_QUESTIONS = 4
+const MAX_QUESTIONS = 50
 
 startGame = () => {
   questionCounter = 0
@@ -67,7 +67,7 @@ getNewQuestion = () => {
   if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
     localStorage.setItem('mostRecentScore', score)
 
-    return window.location.assign('/end.html')
+    return window.location.assign('../quiz/end.html')
   }
 
   questionCounter++
