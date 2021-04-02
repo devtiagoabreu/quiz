3a79ffe29051e7fff6eca3495a8e4f612f1096f1
Tiago de Abreu <devtiagoabreu@gmail.com>
2021-04-02 08:24:34 -0300

    high scores page

--- end.js
+++ end.js
@@ -1,7 +1,7 @@
 const username = document.querySelector('#username')
 const saveScoreBtn = document.querySelector('#saveScoreBtn')
 const finalScore = document.querySelector('#finalScore')
-const mostRecentScore = document.querySelector('#mostRecentScore')
+const mostRecentScore = localStorage.getItem('mostRecentScore')
 
 const highScores = JSON.parse(localStorage.getItem('highScores')) || []
 
@@ -31,7 +31,7 @@ saveHighScore = e => {
   highScores.splice(5)
   
   localStorage.setItem('highScores', JSON.stringify(highScores))
-  window.location.assign('/')
+  window.location.assign('../quiz/highscores.html')
 }
 
 
